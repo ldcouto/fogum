9eff841895dcab8980b0f9f1ff94c59cbd81bfd2
mikelsd <mikelsd@fd0f8ade-3b23-11de-a81c-fde7d73ceb89>
2009-05-12 16:02:56 +0000

    Adicionei classes Deposito.cs,Relatório.cs e Voluntariado.cs.

---
+++ source/FogUM/FogUM/Deposito.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FogUM
+{
+    class Deposito
+    {
+        private int codigo;
+        private float latitude;
+        private float longitude;
+        private float volume;
+        private String descricao;
+
+        public int Cod
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public float Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+
+        public float Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+
+        public float Volume
+        {
+            get { return volume; }
+            set { volume = value; }
+        }
+
+        public String Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+    }
+}
---
+++ source/FogUM/FogUM/Voluntariado.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FogUM
+{
+    class Voluntariado
+    {
+        private int codigo;
+        private String nome;
+        private String morada;
+        private String distrito;
+        private String telefone;
+        private String email;
+
+        public int Cod
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public String Morada
+        {
+            get { return morada; }
+            set { morada = value; }
+        }
+
+        public String Distrito
+        {
+            get { return distrito; }
+            set { distrito = value; }
+        }
+
+        public String Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
