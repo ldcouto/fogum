797c999abd127807ae198257ca249b1f9d17f814
mikelsd <mikelsd@fd0f8ade-3b23-11de-a81c-fde7d73ceb89>
2009-05-12 16:02:56 +0000

    Adicionei classes Deposito.cs,Relatório.cs e Voluntariado.cs.

---
+++ trunk/source/FogUM/FogUM/Relatório.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FogUM
+{
+    public class Relatório
+    {
+        private int codigo;
+        private String comentario;
+
+        public String Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public String Comentario
+        {
+            get { return comentario; }
+            set { comentario = value; }
+        }
+    }
+}
