f35ca7353eb62a66aead78e7801b9dfe53ced387
vasco.gmr@gmail.com <vasco.gmr@gmail.com@fd0f8ade-3b23-11de-a81c-fde7d73ceb89>
2009-05-25 15:51:59 +0000

    Ordens do couto

---
+++ trunk/source/FogUM/FogUM/website/testes/WF1t.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FogUM;
+
+public partial class testes_WF1t : PageBase
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Comandante c1 = new Comandante(1,"","","");
+        Proc_Cmd pc = new Proc_Cmd(c1);
+        Dictionary<int,Corporacao> r = pc.getCoorpDisponiveis();
+        foreach (KeyValuePair<int, Corporacao> kp in r)
+            ListBox1.Items.Add(kp.Value.Nome);
+    }
+}
