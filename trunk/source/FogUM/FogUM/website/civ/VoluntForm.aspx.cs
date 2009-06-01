4a34ddfa18feb12d01bf570d0fbfb84af0887a17
ldcouto <ldcouto@fd0f8ade-3b23-11de-a81c-fde7d73ceb89>
2009-06-01 15:26:26 +0000

    BD e WebCONFIG importantes. NÃO APAGAR!

---
+++ trunk/source/FogUM/FogUM/website/civ/VoluntForm.aspx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Text;
+using FogUM.bd;
+using FogUM;
+
+public partial class VoluntForm : PageBase
+{
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+        
+    }
+
+    public bool IsNumeric(string s)
+    {
+        foreach (char c in s)
+        {
+            if (!char.IsNumber(c))
+                return true;
+        }
+        return false;
+    }
+
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        VOLUNTARIADO vol = new VOLUNTARIADO();
+        DBLinqDataContext bdf = new DBLinqDataContext();
+
+        int fnome = 0, fmail = 0, ftelef = 0, fmorada = 0, disp = 0, distr = 0;
+
+
+           if (!IsNumeric(nome.Text.ToString()) && (nome.Text.ToString().Contains("")))
+               Label17.Visible = true;
+           else
+           {
+               vol.NOME_VOLUNTARIO = nome.Text.ToString();
+               fnome = 1;
+               Label17.Visible = false;
+           }
+
+           if ((!email.Text.ToString().Contains("@")) && (!email.Text.ToString().Contains(".")) && (email.Text.ToString().Contains("")))
+               Label21.Visible = true;
+           else
+           {
+               vol.EMAIL = email.Text.ToString();
+               fmail = 1;
+               Label21.Visible = false;
+           }
+
+           if ((telefone.Text.ToString().Length != 9) || (IsNumeric(telefone.Text.ToString())))
+               Label20.Visible = true;
+           else
+           {
+               vol.NUM_TELEFONE = telefone.Text.ToString();
+               ftelef = 1;
+               Label20.Visible = false;
+           }
+
+           if (morada.Text.ToString().Equals(""))
+               Label19.Visible = true;
+           else
+           {
+               fmorada = 1;
+               Label19.Visible = false;
+           }
+
+           if (DropDownList1.Text.ToString().Equals(""))
+               Label23.Visible = true;
+           else
+           {
+               Label23.Visible = false;
+               distr = 1;
+           }
+
+           if (DropDownList2.Text.ToString().Equals(""))
+               Label22.Visible = true;
+
+           if (DropDownList2.Text.ToString().Equals("3 Meses"))
+           {
+               vol.DISPONIBILIDADE = disp;
+               Label22.Visible = false;
+               disp = 3;
+           }
+           if (DropDownList2.Text.ToString().Equals("6 Meses"))
+           {
+               vol.DISPONIBILIDADE = disp;
+               Label22.Visible = false;
+               disp = 6;
+           }
+           if (DropDownList2.Text.ToString().Equals("12 Meses"))
+           {
+               vol.DISPONIBILIDADE = disp;
+               Label22.Visible = false;
+               disp = 12;
+           }
+
+           if ((fnome == 0) || (fmail == 0) || (ftelef == 0) || (fmorada == 0) || (disp ==0) || (distr==0))
+               WebMsgBox.Show("FORMULÁRIO INVÁLIDO");
+           else
+           {
+               //vol.COD_DISTRITO = convertDistCod(DropDownList1.Text.ToString());
+               //vol.COD_VOLUNTARIO = 1;
+               //vol.COD_DISTRITO = 2;
+               //vol.EMAIL = email.Text.ToString();
+               //vol.NOME_VOLUNTARIO = nome.Text.ToString();
+               //vol.NUM_TELEFONE = telefone.Text.ToString();
+               //vol.DISPONIBILIDADE = disp;
+               //bdf.VOLUNTARIADOs.InsertOnSubmit(vol);
+               //bdf.SubmitChanges();
+               vol.EMAIL = DropDownList1.Text.ToString();
+               WebMsgBox.Show("OBRIGADO PELA SUA COLABORAÇÃO");
+               nome.Text = "";
+               morada.Text = "";
+               telefone.Text = "";
+               email.Text = "";
+               DropDownList2.ClearSelection();
+               DropDownList1.ClearSelection();
+               
+              
+           }
+
+                     
+        
+
+        
+        
+        
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        TextBox1.Text = "";
+    }
+
+}
